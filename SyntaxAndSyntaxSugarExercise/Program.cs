c4ef2ee7fc0bf4d86b2239a72068fdf8d37c5478
jared mckemy <jaredmkcemy4@gmail.com>
2023-08-29 12:25:58 -0700

    ternary/and solution

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            var answer = 4;
+            string var y;
+            y = 4;
+            if (answer < 9)
+            {
+                var = answer + " is less than nine";
+            }
+            else
+            {
+                var = answer + " is greater than or equal to nine";
+            }
         }
     }
 }
