85559fae70e3783d632c9c0479ddbf1c6593a7be
jared mckemy <jaredmkcemy4@gmail.com>
2023-09-19 12:39:20 -0700

    made changes

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -6,16 +6,11 @@
         {
 
             var answer = 4;
-            string var y;
-            y = 4;
-            if (answer < 9)
-            {
-                var = answer + " is less than nine";
-            }
-            else
-            {
-                var = answer + " is greater than or equal to nine";
-            }
+            string y;
+
+            y = answer < 9 ? answer + " is less than nine" : answer + " is greater than or equal to nine";
+
+            Console.WriteLine(y);
         }
     }
 }
